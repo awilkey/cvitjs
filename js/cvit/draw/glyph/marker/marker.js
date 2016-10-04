5effb2b62475c29673166cf7982ba2c993453905
Andrew Wilkey <awilkey@iastate.edu>
2016-10-04 12:56:58 -0500

    marker update

--- js/cvit/draw/glyph/marker/marker.js
+++ js/cvit/draw/glyph/marker/marker.js
@@ -28,36 +28,36 @@ define( [ 'jquery', 'glyph/utilities' ],
        * @param {object} view - object that contains configuration information.
        * @param {paper.group} glyphGroup - Paper group that will hold the positions.
        */
-
-      draw: function( marker, group, view, glyphGroup ) {
-        var target = marker.seqName;
-        var targetGroup = group.children[ target ];
-        if ( targetGroup ) {
-          var yLoc = ( ( marker.start ) * view.yScale ) + targetGroup.children[ target ].bounds.y;
-          var xLoc = ( view.xloc[ target ] + parseInt( view.config.offset ) );
-          var point = new paper.Point( xLoc, yLoc );
-          var size = new paper.Size( parseInt( view.config.width ), 3 );
-          var rectangle = new paper.Rectangle( point, size );
-          var r = new paper.Path.Rectangle( rectangle );
-          if ( parseInt( view.config.enable_pileup ) === 1 ) {
-            this.testCollision( r, glyphGroup, view.pileup );
-          }
-          marker.name = marker.attribute.name ? marker.attribute.name : '';
-          r.info = marker.attribute;
-          r.thisColor = 'black';
-          r.fillColor = utility.formatColor( view.config.color );
-          r.onMouseDown = function( event ) {
-            utility.attachPopover( r, marker );
-          };
-          if ( parseInt( view.config.draw_label ) === 1 ) {
-            var label = utility.generateLabel( marker, view, point, xLoc );
-            targetGroup.addChild( label );
-            glyphGroup.addChild( label );
-            label.bringToFront();
-          }
-          targetGroup.addChild( r );
-          r.sendToBack();
-          glyphGroup.addChild( r );
+       draw: function( marker, group, view, glyphGroup ) {
+         var target = marker.seqName;
+         var targetGroup = group.children[ target ];
+         if ( targetGroup ) {
+           var yLoc = ( ( marker.start ) * view.yScale ) + targetGroup.children[ target ].bounds.y;
+           var xLoc = ( view.xloc[ target ] + parseInt( view.config.offset ) );
+           var point = new paper.Point( xLoc, yLoc );
+           var size = parseInt( view.config.width);
+           var r = new paper.Path.Line( point, new paper.Point(point.x+size, point.y ));
+            r.strokeWidth = 2;
+           if ( parseInt( view.config.enable_pileup ) === 1 ) {
+             this.testCollision( r, glyphGroup, view.pileup );
+           }
+           marker.name = marker.attribute.name ? marker.attribute.name : '';
+           r.info = marker.attribute;
+           r.thisColor = 'black';
+           r.strokeColor = new paper.Color( view.config.color );
+           r.onMouseDown = function( event ) {
+             utility.attachPopover( r, marker );
+           };
+           if ( parseInt( view.config.draw_label ) === 1 ) {
+             point.y = r.position.y;
+             var label = utility.generateLabel( marker, view, point, xLoc );
+             targetGroup.addChild( label );
+             glyphGroup.addChild( label );
+             label.bringToFront();
+           }
+           targetGroup.addChild( r );
+           r.sendToBack();
+           glyphGroup.addChild( r );
         }
       }
     };
